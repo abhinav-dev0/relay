f49c0c927a571108a1097e0a2a4511326df47cb4
abhinav-dev0 <abhinavkbhardwaj@gmail.com>
2026-06-09 13:26:45 +0530

    fixing issues

--- client/src/lib/api.js
+++ client/src/lib/api.js
@@ -2,7 +2,11 @@ import axios from 'axios';
 import { useAuthStore } from '../stores/authStore';
 
 const api = axios.create({
-	baseURL: '/api',
+	baseURL: import.meta.env.VITE_BASE_URL
+		? (import.meta.env.VITE_BASE_URL.endsWith('/api')
+			? import.meta.env.VITE_BASE_URL
+			: `${import.meta.env.VITE_BASE_URL}/api`)
+		: '/api',
 });
 
 api.interceptors.request.use(
