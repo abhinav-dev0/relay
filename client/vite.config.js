1d2eca491910f2b8ee581e7e54e2a68159e2f51b
abhinav-dev0 <abhinavkbhardwaj@gmail.com>
2026-06-09 12:25:00 +0530

    fixing env issue

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,20 +1,20 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
-export default defineConfig({
-	build: {
-		outDir: '../server/src/main/resources/static',
-		emptyOutDir: true,
-	},
-	server: {
-		proxy: {
-			'/api': {
-				target: 'http://localhost:3000',
-				changeOrigin: true,
-				secure: false,
+export default defineConfig(({ mode }) => {
+	const env = loadEnv(mode, process.cwd(), '');
+
+	return {
+		server: {
+			proxy: {
+				'/api': {
+					target: env.VITE_BASE_URL,
+					changeOrigin: true,
+					secure: false,
+				},
 			},
 		},
-	},
-	plugins: [react(), tailwindcss()],
+		plugins: [react(), tailwindcss()],
+	};
 });
